d5569b7932580640e53cc059f79c74a485f1e537
jarotkafka <kafka.albizard17@smk.belajar.id>
2026-02-02 18:53:50 +0700

    save progress sebelum merge gg

--- resources/views/dokumen-perencanaan.blade.php
+++ resources/views/dokumen-perencanaan.blade.php
@@ -6,7 +6,7 @@
     <title>Dokumen Perencanaan</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
         * {
             box-sizing: border-box;
--- resources/views/ppid.blade.php
+++ resources/views/ppid.blade.php
@@ -7,7 +7,7 @@
     <title>PPID</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
         * {
             box-sizing: border-box;
